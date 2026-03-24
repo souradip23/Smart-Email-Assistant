c1a2d3befc704f94ee955a20718b540dda59e866
souradip23 <souradippramanik308@gmail.com>
2026-03-24 19:06:16 +0530

    Smart Email Assistant

---
+++ src/main/java/com/email/writer/SmartEmailAssistantApplication.java
@@ -0,0 +1,14 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmailAssistantApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SmartEmailAssistantApplication.class, args);
+	}
+
+}
